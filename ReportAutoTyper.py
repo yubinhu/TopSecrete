e503e6babd67a048037d0efd6057a4017e0c1851
yubinhu <yubinhu@126.com>
2021-02-21 01:17:23 -0800

    problem

--- ReportAutoTyper.py
+++ ReportAutoTyper.py
@@ -6,7 +6,8 @@ Created on Sun Feb 21 00:41:12 2021
 """
 
 template="""Hi Ms. Fan, how are you doing today madam? The weather in the city of {city} today is going to be mostly {weather}. The high temperature is going to be {temp_max} and the low temperature is going to be {temp_min}. The wind is mostly going to be from the {direction} direction. The speed of the wind is going to be around {wind_speed}. The visibility should be around {visibility}. While the sun rise is expected at {sun_rise}, the sunset is going to be at {sun_set}. Additional information on the position of astronomical bodies in the solar system can be found at https://ssd.jpl.nasa.gov/horizons.cgi
-The high tides in Berkeley Marina is going to be at {high_tide1} and {high_tide2}. The low tides are going to be around {low_tide1}. 
+The high tides in Berkeley Marina are going to be at {high_tide1} and {high_tide2}. The low tide is going to be around {low_tide1}. 
+Hope you have a great day!
 """
 city = "Berkeley"
 weather = "sunny"
